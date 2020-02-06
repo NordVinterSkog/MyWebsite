0782c3ed9d88953919351e63bf0fe920d5a5d50d
Jan <kamilj_95@wp.pl>
2020-02-06 20:27:41 +0100

    add blog and projects

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,20 @@ import React from "react";
 const Contact = () => {
   return (
     <div id="contact" className="contact">
-      CONTACT
+      <div className="aboutme">
+        <img src={require(`./img/me.jpg`)} alt="" />
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+          ullam minima placeat exercitationem. Sint hic cumque molestias, dicta
+          animi architecto laudantium officia e
+        </div>
+      </div>
+      <div className="contactme">
+        <h1>Contact me!</h1>
+        <div className="links">
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
