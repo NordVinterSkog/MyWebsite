8a0fddba8df8246b10b45c84c4fd910407d1ee89
Jan <kamilj_95@wp.pl>
2020-02-13 12:47:57 +0100

    Updates

---
+++ precache-manifest.5e60c4a5519e49c7f10bce1800995628.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fe4ad8c5c47c3bd49735aa9fe4bd01",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "678d9ec1f60dd1a26935",
+    "url": "/MyWebsite/static/css/main.f25c539b.chunk.css"
+  },
+  {
+    "revision": "dd23e5800dd6b2978a74",
+    "url": "/MyWebsite/static/js/2.8aa234f5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/MyWebsite/static/js/2.8aa234f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678d9ec1f60dd1a26935",
+    "url": "/MyWebsite/static/js/main.1dd0389e.chunk.js"
+  },
+  {
+    "revision": "f48482741ea85d31727f",
+    "url": "/MyWebsite/static/js/runtime-main.0e113a3f.js"
+  },
+  {
+    "revision": "9b828f4efa888b32116c2f4fbe6d369a",
+    "url": "/MyWebsite/static/media/0.9b828f4e.jpg"
+  },
+  {
+    "revision": "b544f76cf94c04b10162671331f0c904",
+    "url": "/MyWebsite/static/media/0.b544f76c.jpg"
+  },
+  {
+    "revision": "2fbfaf620b3df272bc45b7db02f7a10f",
+    "url": "/MyWebsite/static/media/1.2fbfaf62.jpg"
+  },
+  {
+    "revision": "f3c3ca80e1ef9b50ec47e8d20c4666e2",
+    "url": "/MyWebsite/static/media/1.f3c3ca80.jpg"
+  },
+  {
+    "revision": "50cf2dc07d0f66e5062e8815dbf02e1a",
+    "url": "/MyWebsite/static/media/2.50cf2dc0.jpg"
+  },
+  {
+    "revision": "1cf032aadc902c22df0257f8ee8c9a64",
+    "url": "/MyWebsite/static/media/aboutme.1cf032aa.jpg"
+  },
+  {
+    "revision": "ac01064407238d383b6a56e17f698946",
+    "url": "/MyWebsite/static/media/blog.ac010644.jpg"
+  },
+  {
+    "revision": "b62a1bca96231d34640afe3601e5183a",
+    "url": "/MyWebsite/static/media/coding.b62a1bca.jpg"
+  },
+  {
+    "revision": "d5f5fbbaa4aadd9951a727118bd6316d",
+    "url": "/MyWebsite/static/media/me.d5f5fbba.jpg"
+  },
+  {
+    "revision": "ab448df54a804528c22a220dae92d0c4",
+    "url": "/MyWebsite/static/media/skog.ab448df5.jpg"
+  }
+]);
